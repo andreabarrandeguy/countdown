09c90d1a0502bc05eb5914883fdb17dd784831ed
Andrea Barrandeguy <abarrandeguy87@hotmail.com>
2024-03-31 20:22:20 +0200

    Improvements on display and experience

--- v4/urls.py
+++ v4/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.indexV4, name="indexV4")
+    path("", views.indexV4, name="indexV4"),
+    path("<int:Y>/<int:m>/<int:d>/<int:H>/<int:M>/<str:title>", views.dynamic, name="dynamic")
 ]
--- v4/views.py
+++ v4/views.py
@@ -1,19 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from datetime import datetime as dt
 
 # Create your views here.
 def indexV4(request):
     if request.method == "POST":
         targetdate = request.POST["targetdate"]
-        td = dt.strptime(targetdate, '%Y-%m-%dT%H:%M')
-        now = dt.now()
+        title = request.POST["title"]
 
-        if td > now:
-            return render(request, "v4/output.html", {
-                "td": targetdate
+        if targetdate and title:
+            td = dt.strptime(targetdate, '%Y-%m-%dT%H:%M')
+            now = dt.now()
+
+            if td > now:
+                # Obtener los componentes de fecha y hora
+                year = td.year
+                month = td.month
+                day = td.day
+                hour = td.hour
+                minute = td.minute
+                title = title
+                
+                # Redirigir al usuario a la página con la ruta dinámica
+                return redirect('dynamic', Y=year, m=month, d=day, H=hour, M=minute, title=title)
+            
+            return render(request, "v4/error.html", {
+            "error": "Please enter a date in the future"
             })
         
-        return render(request, "v4/error.html")
+        return render(request, "v4/error.html", {
+            "error": "Please enter a title and date"
+        })
     
         # Todavia no, pero tengo que generar una view para cuando pare el countdown
     return render(request, "v4/input.html")
+
+
+def dynamic(request, Y, m, d, H, M, title):
+    return render(request, "v4/output.html", {
+        "Y": Y,
+        "m": m,
+        "d": d,
+        "H": H,
+        "M": M,
+        "title": title
+    })
